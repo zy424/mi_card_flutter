c9a6c70c754cb403f2dbdbb1c711b4b32ee6f950
Yi Zhou <zhouyi0424@gmail.com>
2022-04-21 14:41:55 +1200

    My personal mi card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,84 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/zhou.jpg'),
+                ),
+                Text(
+                  'Lily Zhou',
+                  style: TextStyle(
+                      fontSize: 40.0,
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text(
+                  'WEB DEVELOPER',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal.shade100),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text('+64 210674878',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontFamily: 'Source Sans Pro',
+                            color: Colors.teal)),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text('zhouyi0424@gmal.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontFamily: 'Source Sans Pro',
+                            color: Colors.teal)),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
 }
